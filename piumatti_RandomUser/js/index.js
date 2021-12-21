8841d5df09c560b20b26da596f85dad9989059bf
FilippoPiumatti <62744775+FilippoPiumatti@users.noreply.github.com>
2021-12-21 13:13:31 +0100

    Implemented graphics

---
+++ piumatti_RandomUser/js/index.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+window.onload = function () {
+	let slider = $("#myRange");
+	let utenti = $("#lblUtenti");
+	utenti.text("Numero di utenti generati: " + slider.val())
+	slider.on("change",function(){
+		utenti.text("Numero di utenti generati: " + slider.val())
+	})
+    let richiesta = inviaRichiesta("GET", "https://randomuser.me/api?results=5&gender=male");   
+	richiesta.done(function (data) {
+		alert(JSON.stringify(data));
+		console.log(data);		
+	});
+	richiesta.fail(error);
+}
